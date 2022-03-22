4cfbee80394146044febda65cd6a15a8b367240f
EvaTu <evatuw@gmail.com>
2022-03-21 23:20:30 -0400

    class and state

--- monster-app/src/App.js
+++ monster-app/src/App.js
@@ -3,24 +3,25 @@ import './App.css';
 import {Component} from 'react'
 
 class App extends Component {
-
+  constructor(){
+    super()
+    this.state ={
+      name: {firstName: "Eva", lastName: "Tu"},
+      company: "ABC"
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Hello {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}</p>
+          <button onClick={()=> this.setState(()=>{
+            return {
+              name: {firstName: "YiHui", lastName: "Tu"},
+              company: "BBC"
+            }
+            }, ()=> {console.log(this.state)})}>Change Name</button>
         </header>
       </div>
     );
